a4fa1f78d6da74a7c0c9db151343d8622a0cf98b
Tim Au Yeung <kueychuj@gmail.com>
2021-03-30 01:07:51 +0800

    add base path

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 module.exports = {
+  basePath: "/school_demo",
   env: {
     GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
     REPO_FULL_NAME: process.env.REPO_FULL_NAME,
